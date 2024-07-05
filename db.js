b1536b3cfc3ae1c3d3be65af1cf182504a160121
Pradyumna Roy <roypradyumna7@gmail.com>
2024-07-05 18:58:42 +0530

    data base

---
+++ db.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+// Define the MongoDB connection URL
+const mongoURL = `mongodb://127.0.0.1:27017/chefmatch` ;// Replace 'mydatabase' with your database name
+// const mongoURL = process.env.MONGODB_URL;
+
+// Set up MongoDB connection
+mongoose.connect(mongoURL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+
+// Get the default connection
+// Mongoose maintains a default connection object representing the MongoDB connection.
+const db = mongoose.connection;
+
+// Define event listeners for database connection
+
+db.on('connected', () => {
+    console.log('Connected to MongoDB server');
+});
+
+db.on('error', (err) => {
+    console.error('MongoDB connection error:', err);
+});
+
+db.on('disconnected', () => {
+    console.log('MongoDB disconnected');
+});
+
+// Export the database connection
+module.exports = db;
+
--- routes/users.js
+++ routes/users.js
@@ -5,5 +5,35 @@ var router = express.Router();
 router.get('/register', function(req, res, next) {
   res.render("register.ejs");
 });
+router.post("/register",async function(req,res){
+
+    console.log(req.body.name);
+    try{
+        const data = req.body 
+        
+
+        // Check if a user with the same email id already exists
+        const existingUser = await User.findOne({ email: data.email });
+        if (existingUser) {
+            return res.status(400).json({ error: 'User with the same Aadhar Card Number already exists' });
+        }
+
+        // Create a new User document using the Mongoose model
+        const newUser = new User(data);
+
+        // Save the new user to the database
+        const response = await newUser.save();
+        console.log('data saved');
+
+
+        res.redirect('/user/login');
+
+    }
+    catch(err){
+        console.log(err);
+        res.status(500).json({error: 'Internal Server Error'});
+    }
+})
+
 
 module.exports = router;
