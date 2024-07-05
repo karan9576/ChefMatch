fbec2067158be3c822dc067a97bad07829f69fb2
Pradyumna Roy <roypradyumna7@gmail.com>
2024-07-05 19:38:48 +0530

    some update

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,14 @@
 var express = require('express');
 var router = express.Router();
+var bcrypt = require('bcrypt');
 
 /* GET users listing. */
-router.get('/register', function(req, res, next) {
-  res.render("register.ejs");
+router.get('/signup', function(req, res, next) {
+  res.render("signup.ejs");
 });
-router.post("/register",async function(req,res){
+
+
+router.post("/signup",async function(req,res){//check later
 
     console.log(req.body.name);
     try{
@@ -17,6 +20,14 @@ router.post("/register",async function(req,res){
         if (existingUser) {
             return res.status(400).json({ error: 'User with the same Aadhar Card Number already exists' });
         }
+
+        bcrypt.genSalt(saltRounds, function(err, salt) {
+          bcrypt.hash(req.body.password, salt, function(err, hash) {
+              // Store hash in your password DB.
+              data.password = hash;
+              console.log(data);
+          });
+      });
 
         // Create a new User document using the Mongoose model
         const newUser = new User(data);
@@ -34,6 +45,51 @@ router.post("/register",async function(req,res){
         res.status(500).json({error: 'Internal Server Error'});
     }
 })
+
+//login route
+router.get('/login', function(req, res, next) {
+  res.render("login.ejs");
+});
+
+
+router.post('/login', async function(req, res, next) {
+  try{
+    // Extract aadharCardNumber and password from request body
+    const {email, password} = req.body;
+
+    // Check if aadharCardNumber or password is missing
+    if (!email || !password) {
+        return res.status(400).json({ error: 'email and password are required' });
+    }
+
+    // Find the user by aadharCardNumber
+    const user = await User.findOne({email: email});
+
+    // If user does not exist or password does not match, return error
+    if( !user || !(await user.comparePassword(password))){
+        return res.status(401).json({error: 'email or Password'});
+    }
+
+    // generate Token //
+    const payload = {
+        id: user.id,
+    }
+    const token = generateToken(payload);
+    console.log("succesful login");
+
+    res.cookie('token', token);
+
+    // return token as response..
+    res.redirect('/');
+    // res.json({token})
+}catch(err){
+    console.error(err);
+    res.status(500).json({ error: 'Internal Server Error'});
+}
+});
+
+
+
 
 
 module.exports = router;
